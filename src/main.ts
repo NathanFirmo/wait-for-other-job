cc88b9a235f0e1cbfd8e9c6dd29677c47b66f348
NathanFirmo <nathanssfirmo@gmail.com>
2023-03-28 12:36:28 -0300

    feat: add main functions

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,65 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import { Octokit } from '@octokit/rest'
 
 async function run(): Promise<void> {
+  const job: string = core.getInput('job', {
+    required: true,
+    trimWhitespace: true
+  })
+
+  const failOnJobError: boolean = core.getBooleanInput('failOnJobError', {
+    required: false
+  })
+
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    core.info(`Waiting until ${job} finish`)
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    let jobToWait = await getStatusOfJob(job)
 
-    core.setOutput('time', new Date().toTimeString())
+    while (jobToWait?.status === 'in_progress') {
+      await new Promise(resolve => setTimeout(resolve, 3000))
+      jobToWait = await getStatusOfJob(job)
+    }
+
+    if (jobToWait?.conclusion === 'failure' && failOnJobError) {
+      throw new Error(`The job ${job} have failed`)
+    }
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) {
+      if (error.message === 'Not Found') {
+        core.error(
+          `It seems the job "${job}" doesn't exist in current repo. Are this filename correct?`
+        )
+      }
+      core.setFailed(error.message)
+    }
   }
 }
 
 run()
+
+async function getStatusOfJob(jobName: string) {
+  const token: string = core.getInput('token', {
+    required: true,
+    trimWhitespace: true
+  })
+
+  const { context } = github
+
+  const octokit = new Octokit({
+    auth: token
+  })
+
+  const {
+    data: { jobs }
+  } = await octokit.actions.listJobsForWorkflowRunAttempt({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    run_id: context.runId,
+    attempt_number: context.runNumber
+  })
+
+  const jobToWait = jobs.find(job => job.name === jobName)
+  return jobToWait
+}
